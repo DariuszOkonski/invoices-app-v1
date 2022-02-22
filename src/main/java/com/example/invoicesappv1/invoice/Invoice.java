2461fb39dd824460ac6c17a809f9b99b06712eee
Dariusz Okonski <darek200180@gmail.com>
2022-02-22 03:27:03 +0000

    add constructors and getters, setters to class Invoice

--- src/main/java/com/example/invoicesappv1/invoice/Invoice.java
+++ src/main/java/com/example/invoicesappv1/invoice/Invoice.java
@@ -10,4 +10,62 @@ public class Invoice {
     private LocalDate createdAt;
     private Supplier supplier;
     private BigDecimal amount;
+
+    public Invoice() {
+    }
+
+    public Invoice(Long id, LocalDate createdAt, Supplier supplier, BigDecimal amount) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.supplier = supplier;
+        this.amount = amount;
+    }
+
+    public Invoice(LocalDate createdAt, Supplier supplier, BigDecimal amount) {
+        this.createdAt = createdAt;
+        this.supplier = supplier;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "id=" + id +
+                ", createdAt=" + createdAt +
+                ", supplier=" + supplier +
+                ", amount=" + amount +
+                '}';
+    }
 }
