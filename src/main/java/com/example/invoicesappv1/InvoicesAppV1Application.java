2ec8bf8e67596937f540e05a2984733f2e3c904f
Dariusz Okonski <darek200180@gmail.com>
2022-02-22 03:01:47 +0000

    initialization

---
+++ src/main/java/com/example/invoicesappv1/InvoicesAppV1Application.java
@@ -0,0 +1,13 @@
+package com.example.invoicesappv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicesAppV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvoicesAppV1Application.class, args);
+	}
+
+}
